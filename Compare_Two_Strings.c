4d1ff583a5754afbd9360e32cba1bf48a4d5437f
ganjasrija <147472787+ganjasrija@users.noreply.github.com>
2024-02-14 11:14:06 +0530

    #include<stdio.h> #include<string.h> int main(){     int c;     char s1[1000];     char s2[1000];     scanf("%s%s",s1,s2);     c=strcmp(s1,s2);     if(c==0){         printf("Strings are Equal");
        }
        else{
            printf("Strings are not Equal");
        }
    }

---
+++ Compare_Two_Strings.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    int c;
+    char s1[1000];
+    char s2[1000];
+    scanf("%s%s",s1,s2);
+    c=strcmp(s1,s2);
+    if(c==0){
+        printf("Strings are Equal");
+        
+    }
+    else{
+        printf("Strings are not Equal");
+    }
+}
