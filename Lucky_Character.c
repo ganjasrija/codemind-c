f6411d67c28e3891ddbb15363626ae5433aeffab
ganjasrija <147472787+ganjasrija@users.noreply.github.com>
2024-02-14 12:02:18 +0530

    #include<stdio.h> #include<string.h> int main(){     char st[10];     int i;     char c;     scanf("%s",st);     for(i=0;st[i]!=NULL;i++){        c= st[6];        break;     }     printf("%c",c); }

---
+++ Lucky_Character.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<string.h>
+int main(){
+    char st[10];
+    int i;
+    char c;
+    scanf("%s",st);
+    for(i=0;st[i]!=NULL;i++){
+       c= st[6];
+       break;
+    }
+    printf("%c",c);
+}
