aa06705453ea9afdceb59a8aef043275cbd96c48
ganjasrija <147472787+ganjasrija@users.noreply.github.com>
2024-02-19 21:01:23 +0530

    #include<stdio.h> int main(){     int m,n,i,j,sum;     scanf("%d%d",&m,&n);     int A[100][100];     for(i=0;i<m;i++){
            for(j=0;j<n;j++){
                scanf("%d",&A[i][j]);
            }
        }
        for(i=0;i<m;i++){
            
            for(j=0;j<n;j++){
                sum=sum+A[i][j];
                
        
            }
            printf("%d ",sum);
            sum=0;
        }
    
    }

---
+++ Row_wise_sum_of_a_matrix.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+int main(){
+    int m,n,i,j,sum;
+    scanf("%d%d",&m,&n);
+    int A[100][100];
+    for(i=0;i<m;i++){
+        
+        for(j=0;j<n;j++){
+            scanf("%d",&A[i][j]);
+        }
+    }
+    for(i=0;i<m;i++){
+        
+        for(j=0;j<n;j++){
+            sum=sum+A[i][j];
+            
+    
+        }
+        printf("%d ",sum);
+        sum=0;
+    }
+
+}
